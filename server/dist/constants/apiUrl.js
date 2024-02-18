23deefc335a159eb3affaae615a3919e562c8f9f
pramoddivekar31 <68549426+pramoddivekar31@users.noreply.github.com>
2024-02-18 20:36:14 +0530

    Delete server/dist directory

--- server/dist/constants/apiUrl.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SEARCH_BASE_URL = void 0;
-exports.SEARCH_BASE_URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsort=relevance";
-//# sourceMappingURL=apiUrl.js.map
--- server/dist/constants/appRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Routes;
-(function (Routes) {
-    Routes["Search"] = "/search";
-})(Routes || (Routes = {}));
-exports.default = Routes;
-//# sourceMappingURL=appRoutes.js.map
